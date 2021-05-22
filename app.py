aedef70898550a48773ba76dd5c654ac047bbf30
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-05-22 19:55:14 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask import render_template
 app = Flask(__name__)
 
@@ -15,3 +15,16 @@ def hello():
 @app.route('/hello/<name>')
 def hello2(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
